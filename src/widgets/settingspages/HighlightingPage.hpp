bb5b6a40ddb9bc8d25751e7805b604f72b394fc2
CycloneTM <ianbuddy278@gmail.com>
2023-05-11 21:54:10 -0500

    Update HighlightingPage.hpp

--- src/widgets/settingspages/HighlightingPage.hpp
+++ src/widgets/settingspages/HighlightingPage.hpp
@@ -28,7 +28,7 @@ private:
                          int soundColumn);
     void openColorDialog(const QModelIndex &clicked, EditableModelView *view,
                          HighlightTab tab);
-                         
+
     void setBoldOnHighlight(const QModelIndex &index, bool isBold);
 };
 
